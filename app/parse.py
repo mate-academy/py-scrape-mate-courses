406745ca11a8cda1fd87d2d932f6f3d65ad9af90
Taras Nester <tarasnester414@gmail.com>
2022-07-25 17:15:24 +0300

    add getting basic course info

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,10 @@
+import requests
 from dataclasses import dataclass
 from enum import Enum
+from bs4 import BeautifulSoup
+
+
+BASE_URL = "https://mate.academy/"
 
 
 class CourseType(Enum):
@@ -14,5 +19,34 @@ class Course:
     type: CourseType
 
 
+def get_single_course(course, course_type: CourseType):
+    return Course(
+            name=course.select_one("span.typography_landingH3__vTjok")
+            .text.replace("Курс ", "")
+            .replace(" Вечірній", ""),
+            short_description=course.select_one("p").text,
+            type=course_type
+
+        )
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    page = requests.get(BASE_URL).content
+    soup = BeautifulSoup(page, "html.parser")
+
+    result = []
+    for course_type in CourseType:
+        course_block = soup.select_one(f"#{course_type.value}")
+        courses = course_block.select("section.CourseCard_cardContainer__7_4lK")
+        for course in courses:
+            result.append(get_single_course(course, course_type))
+
+    return result
+
+
+def main():
+    print(get_all_courses())
+
+
+if __name__ == '__main__':
+    main()
