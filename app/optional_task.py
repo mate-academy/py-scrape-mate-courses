0ac0873197217cb6b88394b62d9ccd0b962e22c2
Anton Lasko <anton.lasko1993@gmail.com>
2022-09-14 19:52:34 +0300

    'Solution'

---
+++ app/optional_task.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+root = "https://mate.academy/"
+website = f'{root}/courses'
+result = requests.get(website)
+content = result.text
+soup = BeautifulSoup(content, "html.parser")
+
+box = soup.find(class_='cell large-6 large-offset-1 mb-32')
+
+links = [link['href'] for link in box.find_all('a', class_='mb-16', href=True)]
+
+for link in links:
+    result = requests.get(f'{root}/{link}')
+    content = result.text
+    soup = BeautifulSoup(content, "html.parser")
+
+    name = str(link).split("/")[-1]
+    num_modules = soup.select_one(".CourseModulesHeading_modulesNumber__GNdFP > p").text
+    num_topics = soup.select_one(".CourseModulesHeading_topicsNumber__PXMnR > p").text
+    duration = soup.select_one(".CourseModulesHeading_courseDuration__f_c3H > p").text
+
+    print(f"{name}: {num_modules}, {num_topics}, {duration}")
--- app/parse.py
+++ app/parse.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
 from enum import Enum
 
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://mate.academy/"
+
 
 class CourseType(Enum):
     FULL_TIME = "full-time"
@@ -14,5 +19,20 @@ class Course:
     type: CourseType
 
 
+def parse_single_course(course_soup) -> Course:
+    print(dict(
+        name=course_soup.select_one(".typography_landingH3__vTjok").text,
+        short_description=course_soup.select_one(".CourseCard_flexContainer__dJk4p > p").text,
+        type="part-time" if "Вечірній" in str(course_soup.select_one(".typography_landingH3__vTjok").text) else
+        "full-time"
+    ))
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    page = requests.get(URL).content
+    soup = BeautifulSoup(page, "html.parser")
+    courses = soup.select(".CourseCard_cardContainer__7_4lK")
+    return [parse_single_course(course_soup) for course_soup in courses]
+
+
+get_all_courses()
