5e4191b6d00bdb56da03a5c22e33e611b569376b
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-10-27 20:54:58 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -24,6 +24,21 @@ class Course:
     duration: str
 
 
+def get_single_course(course: BeautifulSoup, course_type: CourseType) -> Course:
+    name = course.select_one(".typography_landingH3__vTjok").text
+    short_description = course.select_one(".CourseCard_courseDescription__Unsqj").text
+    course_link = course.select_one(".CourseCard_button__HTQvE").get("href")
+    course_url = urljoin(BASE_URL, course_link)
+    request = requests.get(course_url).content
+    soup = BeautifulSoup(request, "html.parser")
+    modules = soup.select_one(".CourseModulesHeading_modulesNumber__GNdFP").text
+    topics = soup.select_one(".CourseModulesHeading_topicsNumber__PXMnR").text
+    duration = None
+    if course_type == CourseType.FULL_TIME:
+        duration = soup.select_one(".CourseModulesHeading_courseDuration__f_c3H").text
+    return Course(name, short_description, course_type, modules, topics, duration)
+
+
 def get_courses_by_course_type(course_type: CourseType) -> list[Course]:
     courses_list = []
     request = requests.get(BASE_URL).content
@@ -31,18 +46,7 @@ def get_courses_by_course_type(course_type: CourseType) -> list[Course]:
     soup_type = soup.find(id=course_type.value)
     courses = soup_type.select(".CourseCard_cardContainer__7_4lK")
     for course in courses:
-        name = course.select_one(".typography_landingH3__vTjok").text
-        short_description = course.select_one(".CourseCard_courseDescription__Unsqj").text
-        course_link = soup_type.select_one(".CourseCard_button__HTQvE").get("href")
-        course_url = urljoin(BASE_URL, course_link)
-        request = requests.get(course_url).content
-        soup = BeautifulSoup(request, "html.parser")
-        modules = soup.select_one(".CourseModulesHeading_modulesNumber__GNdFP").text
-        topics = soup.select_one(".CourseModulesHeading_topicsNumber__PXMnR").text
-        duration = None
-        if course_type == CourseType.FULL_TIME:
-            duration = soup.select_one(".CourseModulesHeading_courseDuration__f_c3H").text
-        courses_list.append(Course(name, short_description, course_type, modules, topics, duration))
+        courses_list.append(get_single_course(course, course_type))
     return courses_list
 
 
