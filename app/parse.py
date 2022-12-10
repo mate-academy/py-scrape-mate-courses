5580659189fc3d1791d306187163d01392f9bbe8
Dmytro Skrypka <2346677tyu@gmail.com>
2022-12-10 23:10:41 +0200

    Added scrap for name courses

--- app/parse.py
+++ app/parse.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
 from enum import Enum
 
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://mate.academy/"
+
 
 class CourseType(Enum):
     FULL_TIME = "full-time"
@@ -14,5 +19,23 @@ class Course:
     course_type: CourseType
 
 
+def parse_single_course(course_soup: BeautifulSoup) -> Course:
+    print(dict(
+        name=course_soup.select_one(".typography_landingH3__vTjok"),
+    ))
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    page = requests.get(BASE_URL).content
+    soup = BeautifulSoup(page, "html.parser")
+    courses = soup.select(".CourseCard_cardContainer__7_4lK")
+
+    return [parse_single_course(course_soup) for course_soup in courses]
+
+
+def main():
+    get_all_courses()
+
+
+if __name__ == '__main__':
+    main()
