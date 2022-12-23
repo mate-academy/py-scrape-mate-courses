598857a5a2ea9d4b25ff06dfeb44ee9c530c5682
Alexander <alexzubkoff123@gmail.com>
2022-12-23 23:31:10 +0200

    Added parsier for mate courses using Requests and bs4

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 from enum import Enum
+import requests
+from bs4 import BeautifulSoup
 
 
 class CourseType(Enum):
@@ -14,5 +16,42 @@ class Course:
     course_type: CourseType
 
 
-def get_all_courses() -> list[Course]:
-    pass
+BASE_URL = "https://mate.academy"
+
+
+def parse_single_type_courses(
+        courses_soup: BeautifulSoup,
+        course_type: str
+) -> [Course]:
+    courses = []
+    for item_soup in courses_soup:
+        course = Course(
+            name=item_soup.select("span.typography_landingH3__vTjok")[0].text,
+            short_description=item_soup.select(
+                "p.typography_landingP1__N9PXd"
+            )[0].text,
+            course_type=course_type
+        )
+        courses.append(course)
+    return courses
+
+
+def get_all_courses() -> [Course]:
+    page = requests.get(BASE_URL).content
+    soup = BeautifulSoup(page, "html.parser")
+    full_time_soup = soup.select("#full-time section")
+    part_time_soup = soup.select("#part-time section")
+    full_time_courses = parse_single_type_courses(
+        full_time_soup,
+        CourseType.FULL_TIME
+    )
+    part_time_courses = parse_single_type_courses(
+        part_time_soup,
+        CourseType.PART_TIME
+    )
+    all_mate_courses = full_time_courses + part_time_courses
+    return all_mate_courses
+
+
+if __name__ == "__main__":
+    print(get_all_courses())
