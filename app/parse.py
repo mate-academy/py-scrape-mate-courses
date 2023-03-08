f6e1d83eb98cff9a66cc4970834fa662e0e8144a
eugene <bantyc8@gmail.com>
2023-03-08 18:16:56 +0200

    solution with clear code

--- app/parse.py
+++ app/parse.py
@@ -30,26 +30,42 @@ def get_detail_course(course_soup: BeautifulSoup) -> BeautifulSoup:
 
 
 def parse_single_course(course_soup: BeautifulSoup) -> Course:
-    name = course_soup.select_one(".typography_landingH3__vTjok").text.strip("Курс")[1:]
-    short_description = course_soup.select_one(".CourseCard_flexContainer__dJk4p").text.strip("Детальніше")
+    name = course_soup.select_one(
+        ".typography_landingH3__vTjok"
+    ).text.strip("Курс")[1:]
+    short_description = course_soup.select_one(
+        ".CourseCard_flexContainer__dJk4p"
+    ).text.strip("Детальніше")
     course_type = (
         CourseType.PART_TIME if "Вечірній" in name else CourseType.FULL_TIME
     )
     print(course_type, name)
     detail_page = get_detail_course(course_soup)
-    modules = detail_page.select_one(".CourseModulesHeading_modulesNumber__GNdFP").text
-    topic = detail_page.select_one(".CourseModulesHeading_topicsNumber__PXMnR").text
+    modules = detail_page.select_one(
+        ".CourseModulesHeading_modulesNumber__GNdFP"
+    ).text
+    topic = detail_page.select_one(
+        ".CourseModulesHeading_topicsNumber__PXMnR"
+    ).text
     duration = (
         str(
             detail_page.select_one(
-                ".CourseModulesHeading_courseDuration__f_c3H > p.CourseModulesHeading_text__EdrEk"
+                (".CourseModulesHeading_courseDuration__f_c3H >"
+                 " p.CourseModulesHeading_text__EdrEk")
             ).text
         )
         if course_type == CourseType.FULL_TIME
         else "Навчайся у власному графіку"
     )
 
-    return Course(name, short_description, course_type, modules, topic, duration)
+    return Course(
+        name,
+        short_description,
+        course_type,
+        modules,
+        topic,
+        duration
+    )
 
 
 def get_all_courses() -> list[Course]:
