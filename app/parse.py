d1f9a7afeb1581145a3bedcb85a04989a667094c
George <georgepavlej@gmail.com>
2023-04-01 17:35:33 +0100

    PEP updates

--- app/parse.py
+++ app/parse.py
@@ -22,7 +22,8 @@ class Course:
 def get_course_info(course_section: Tag, course_type: CourseType) -> Course:
     name = course_section.find(class_="typography_landingH3__vTjok").text
     short_description = course_section.find(
-        class_="typography_landingP1__N9PXd").text
+        class_="typography_landingP1__N9PXd"
+    ).text
 
     return Course(name, short_description, course_type)
 
@@ -34,8 +35,10 @@ def get_courses_by_type(course_type: CourseType) -> list[Course]:
         id=course_type.value
     ).find_all(class_="CourseCard_cardContainer__7_4lK")
 
-    return [get_course_info(
-        course_section, course_type) for course_section in course_sections]
+    return [
+        get_course_info(course_section, course_type)
+        for course_section in course_sections
+    ]
 
 
 def get_all_courses() -> list[Course]:
