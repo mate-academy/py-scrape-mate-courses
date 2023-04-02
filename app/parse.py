03242f4491a7fc3e67ae17db29820922267f0ea4
Inna Kushnir <innakushnir63@gmail.com>
2023-04-02 21:47:08 +0300

    Flake8

--- app/parse.py
+++ app/parse.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import List, Tuple
 import requests
 from bs4 import BeautifulSoup, Tag
 
@@ -22,7 +21,9 @@ class Course:
 def parse_course(course_soup: Tag, course_type: CourseType) -> Course:
     return Course(
         name=course_soup.select_one(".typography_landingH3__vTjok").text,
-        short_description=course_soup.select_one(".typography_landingP1__N9PXd").text,
+        short_description=course_soup.select_one(
+            ".typography_landingP1__N9PXd"
+        ).text,
         course_type=course_type,
     )
 
@@ -30,7 +31,9 @@ def parse_course(course_soup: Tag, course_type: CourseType) -> Course:
 def get_full_time_courses() -> list[Course]:
     page = requests.get(HOME_URL).content
     soup = BeautifulSoup(page, "html.parser")
-    full_time_courses = soup.select("#full-time .CourseCard_cardContainer__7_4lK")
+    full_time_courses = soup.select(
+        "#full-time .CourseCard_cardContainer__7_4lK"
+    )
     return [
         parse_course(course_soup, CourseType.FULL_TIME)
         for course_soup in full_time_courses
@@ -40,7 +43,9 @@ def get_full_time_courses() -> list[Course]:
 def get_part_time_courses() -> list[Course]:
     page = requests.get(HOME_URL).content
     soup = BeautifulSoup(page, "html.parser")
-    part_time_courses = soup.select("#part-time .CourseCard_cardContainer__7_4lK")
+    part_time_courses = soup.select(
+        "#part-time .CourseCard_cardContainer__7_4lK"
+    )
     return [
         parse_course(course_soup, CourseType.FULL_TIME)
         for course_soup in part_time_courses
