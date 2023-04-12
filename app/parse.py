f16d1a8b86d5064829afde0b27024623b8240ef7
Maksym_Sylantiev <maxym2s@yahoo.com>
2023-04-12 21:46:12 +0300

    Updated code with DRY

--- app/parse.py
+++ app/parse.py
@@ -1,9 +1,8 @@
-from bs4 import BeautifulSoup, Tag
+import requests
 
+from bs4 import BeautifulSoup, Tag
 from dataclasses import dataclass
 from enum import Enum
-
-import requests
 
 
 HOME_URL = "https://mate.academy/"
@@ -21,45 +20,30 @@ class Course:
     course_type: CourseType
 
 
-def parse_single_full_time_course(course_soup: Tag) -> Course:
+def parse_single_course(course_soup: Tag, course_type: CourseType) -> Course:
     return Course(
         name=course_soup.select_one(".typography_landingH3__vTjok").text,
         short_description=course_soup.select_one(
             ".typography_landingP1__N9PXd"
         ).text,
-        course_type=CourseType.FULL_TIME
+        course_type=course_type
     )
 
 
-def parse_single_part_time_course(course_soup: Tag) -> Course:
-    return Course(
-        name=course_soup.select_one(".typography_landingH3__vTjok").text,
-        short_description=course_soup.select_one(
-            ".typography_landingP1__N9PXd"
-        ).text,
-        course_type=CourseType.PART_TIME
-    )
-
-
-def get_full_time_courses() -> list[Course]:
+def get_courses(course_type: CourseType) -> list[Course]:
     page = requests.get(HOME_URL).content
     soup = BeautifulSoup(page, "html.parser")
-    full_time_courses = soup.select(
-        "#full-time .CourseCard_cardContainer__7_4lK"
+    courses = soup.select(
+        f"#{course_type.value} .CourseCard_cardContainer__7_4lK"
     )
-    return [parse_single_full_time_course(course_soup)
-            for course_soup in full_time_courses]
-
-
-def get_part_time_courses() -> list[Course]:
-    page = requests.get(HOME_URL).content
-    soup = BeautifulSoup(page, "html.parser")
-    part_time_courses = soup.select(
-        "#part-time .CourseCard_cardContainer__7_4lK"
-    )
-    return [parse_single_part_time_course(course_soup)
-            for course_soup in part_time_courses]
+    return [
+        parse_single_course(course_soup, course_type)
+        for course_soup in courses
+    ]
 
 
 def get_all_courses() -> list[Course]:
-    return [*get_full_time_courses(), *get_part_time_courses()]
+    return [
+        *get_courses(CourseType.FULL_TIME),
+        *get_courses(CourseType.PART_TIME),
+    ]
