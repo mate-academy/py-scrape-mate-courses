9b482919f258971cd27ddde83256ca8e495dc636
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2023-04-13 23:37:33 +0200

    Revert "Solution"
    This reverts commit f54761643c506bfd494f9e101f2512749fd74acb.

--- app/parse.py
+++ app/parse.py
@@ -1,9 +1,5 @@
 from dataclasses import dataclass
 from enum import Enum
-import requests
-from bs4 import BeautifulSoup, element
-
-MATE_URL = "https://mate.academy/"
 
 
 class CourseType(Enum):
@@ -13,27 +9,10 @@ class CourseType(Enum):
 
 @dataclass
 class Course:
-    name: tuple
-    short_description: tuple
+    name: str
+    short_description: str
     course_type: CourseType
 
 
-def parse_single_course(course_soup: element.Tag) -> Course:
-    name = course_soup.select_one("span").text,
-    short_description = course_soup.select_one("p").text,
-    course_type = (
-        CourseType.PART_TIME if "Вечірній" in name[0].split()
-        else CourseType.FULL_TIME
-    )
-    return Course(
-        name=name, short_description=short_description, course_type=course_type
-    )
-
-
 def get_all_courses() -> list[Course]:
-    page = requests.get(MATE_URL).content
-    soup = BeautifulSoup(page, "html.parser")
-
-    courses = soup.select(".CourseCard_cardContainer__7_4lK")
-
-    return [parse_single_course(course_soup) for course_soup in courses]
+    pass
