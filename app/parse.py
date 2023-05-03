8ec3372594637ebc954b88d0345eb3b389140520
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-05-03 20:27:58 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,11 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
 from dataclasses import dataclass
 from enum import Enum
+
+
+URL = "https://mate.academy/"
 
 
 class CourseType(Enum):
@@ -14,5 +20,42 @@ class Course:
     course_type: CourseType
 
 
+def parse_single_course(
+        course_soup: BeautifulSoup,
+        course_type: CourseType
+) -> Course:
+    return Course(
+        name=course_soup.select_one("a", {"class": "mb-16"}).text,
+        short_description=(
+            course_soup.select_one(
+                "p", {"class": "typography_"}, partial=True).text
+        ),
+        course_type=course_type
+    )
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    driver = webdriver.Chrome()
+    driver.get(URL)
+    page = driver.page_source
+    soup = BeautifulSoup(page, "html.parser")
+    courses_list = []
+
+    for course_type in CourseType:
+        section = soup.find("div", {"id": course_type.value})
+        courses = section.findAll(
+            "section", {"class": "CourseCard_cardContainer__7_4lK"}
+        )
+
+        for course in courses:
+            courses_list.append(parse_single_course(course, course_type))
+
+    return courses_list
+
+
+def main():
+    get_all_courses()
+
+
+if __name__ == "__main__":
+    main()
