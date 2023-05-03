955463c221f6d1760987b42173f27ace0e0df04f
Dmytry <donchykd@gmail.com>
2023-05-03 15:05:39 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
 from enum import Enum
 
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://mate.academy/"
+
 
 class CourseType(Enum):
     FULL_TIME = "full-time"
@@ -14,5 +19,26 @@ class Course:
     course_type: CourseType
 
 
+def parse_single_full_time_course(course_soup: BeautifulSoup) -> Course:
+    return Course(
+        name=course_soup.select_one(".typography_landingH3__vTjok").text,
+        short_description=course_soup.select_one(".CourseCard_courseDescription__Unsqj").text,
+        course_type=CourseType.FULL_TIME,
+    )
+
+
+def parse_single_part_time_course(course_soup: BeautifulSoup) -> Course:
+    return Course(
+        name=course_soup.select_one(".typography_landingH3__vTjok").text,
+        short_description=course_soup.select_one(".CourseCard_courseDescription__Unsqj").text,
+        course_type=CourseType.PART_TIME,
+    )
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    page = requests.get(BASE_URL).content
+    soup = BeautifulSoup(page, "html.parser")
+    courses = soup.select(".CourseCard_cardContainer__7_4lK")
+    courses_list_full_time = [parse_single_full_time_course(course_soup) for course_soup in courses]
+    courses_list_part_time = [parse_single_part_time_course(course_soup) for course_soup in courses]
+    return courses_list_full_time + courses_list_part_time
