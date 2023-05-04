953a3c63d5e0755d382ff0c6f0635df5b5060315
Dmytry <donchykd@gmail.com>
2023-05-04 11:52:13 +0300

    Drying code

--- app/parse.py
+++ app/parse.py
@@ -19,23 +19,13 @@ class Course:
     course_type: CourseType
 
 
-def parse_single_full_time_course(course_soup: BeautifulSoup) -> Course:
+def parse_single_course(course_soup: BeautifulSoup, time: str) -> Course:
     return Course(
         name=course_soup.select_one(".typography_landingH3__vTjok").text,
         short_description=course_soup.select_one(
             ".CourseCard_courseDescription__Unsqj"
         ).text,
-        course_type=CourseType.FULL_TIME,
-    )
-
-
-def parse_single_part_time_course(course_soup: BeautifulSoup) -> Course:
-    return Course(
-        name=course_soup.select_one(".typography_landingH3__vTjok").text,
-        short_description=course_soup.select_one(
-            ".CourseCard_courseDescription__Unsqj"
-        ).text,
-        course_type=CourseType.PART_TIME,
+        course_type=time,
     )
 
 
@@ -43,12 +33,13 @@ def get_all_courses() -> list[Course]:
     page = requests.get(BASE_URL).content
     soup = BeautifulSoup(page, "html.parser")
     courses = soup.select(".CourseCard_cardContainer__7_4lK")
-    courses_list_full_time = [
-        parse_single_full_time_course(course_soup)
-        for course_soup in courses
-    ]
-    courses_list_part_time = [
-        parse_single_part_time_course(course_soup)
-        for course_soup in courses
-    ]
-    return courses_list_full_time + courses_list_part_time
+    times = [CourseType.FULL_TIME, CourseType.PART_TIME]
+    result = []
+    for time in times:
+        courses_list_time = [
+            parse_single_course(course_soup, time)
+            for course_soup in courses
+        ]
+        result += courses_list_time
+
+    return result
