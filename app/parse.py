60cd075f6bf08ecc2bf107109aeba0fbf207e98e
Arsen Makovei <makoveyarsen@gmail.com>
2023-05-05 11:59:33 +0300

    Added Selenium webdriver

--- app/parse.py
+++ app/parse.py
@@ -4,6 +4,9 @@ from urllib.parse import urljoin
 
 import requests
 from bs4 import BeautifulSoup, Tag
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 
 BASE_URL = "https://mate.academy/"
@@ -31,8 +34,11 @@ def parse_single_course(course_soup: Tag, course_type: CourseType) -> Course:
     detail_page = requests.get(urljoin(BASE_URL, detail_href)).content
     detail_soup = BeautifulSoup(detail_page, "html.parser")
 
+    duration_elem = detail_soup.select_one("div.CourseModulesHeading_courseDuration__f_c3H")
+    months_duration = duration_elem.text if duration_elem is not None else None
+
     return Course(
-        name=course_soup.select_one(".typography_landingH3__vTjok").text,
+        name=course_soup.select_one("span.typography_landingH3__vTjok").text,
         short_description=course_soup.select_one(
             "p.CourseCard_courseDescription__Unsqj"
         ).text,
@@ -43,14 +49,18 @@ def parse_single_course(course_soup: Tag, course_type: CourseType) -> Course:
         num_topics=detail_soup.select_one(
             "div.CourseModulesHeading_topicsNumber__PXMnR"
         ).text,
-        months_duration=detail_soup.select_one(
-            "div.CourseModulesHeading_courseDuration__f_c3H"
-        ).text,
+        months_duration=months_duration,
     )
 
 
 def get_courses_by_type(course_type: CourseType) -> list[Course]:
-    page = requests.get(BASE_URL).content
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    service = Service("/usr/lib/chromium-browser/chromedriver")
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.get(BASE_URL)
+    page = driver.page_source
     soup = BeautifulSoup(page, "html.parser")
     courses_soup = soup.select(f"#{course_type.value} section")
 
@@ -68,4 +78,5 @@ def get_all_courses() -> list[Course]:
 
 
 if __name__ == "__main__":
-    print(get_all_courses())
+    for course in get_all_courses():
+        print(course)
