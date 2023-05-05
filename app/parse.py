bc5a480f7bfce23bcb0e8f6d719f4b3a24cd98cb
Yulia <Mala>
2023-05-05 12:48:06 +0300

    driver fixed

--- app/parse.py
+++ app/parse.py
@@ -2,7 +2,8 @@ from dataclasses import dataclass
 from enum import Enum
 from bs4 import BeautifulSoup, Tag
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 COURSES_URL = "https://mate.academy/"
 
@@ -29,17 +30,17 @@ def parse_one_course(item: Tag) -> Course:
         short_description=item.select_one(
             ".typography_landingMainText__Ux18x"
         ).text,
-        course_type=course_type
-    )
+        course_type=course_type)
 
 
 def get_all_courses() -> list[Course]:
-    driver = webdriver.Chrome()
-    driver.set_page_load_timeout(60)
-
+    service = Service("/usr/local/bin/chromedriver")
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(service=service, options=options)
     driver.get(COURSES_URL)
     source = BeautifulSoup(driver.page_source, "html.parser")
     courses = source.select(".CourseCard_cardContainer__7_4lK")
-    driver.close()
+    driver.quit()
 
     return [parse_one_course(course) for course in courses]
