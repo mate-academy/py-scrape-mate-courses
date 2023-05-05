7c11d4b55839ce722098e7f1415781e03c6cf73c
Yevhen Kopatko <cs.kopatko.evgen@gmail.com>
2023-05-05 09:19:16 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,12 @@
+import json
 from dataclasses import dataclass
 from enum import Enum
+from typing import List
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://mate.academy/"
 
 
 class CourseType(Enum):
@@ -14,5 +21,37 @@ class Course:
     course_type: CourseType
 
 
-def get_all_courses() -> list[Course]:
-    pass
+def parse_single_course(course_dict: dict) -> Course:
+    name = course_dict['nameShort({"domain":"ua","lang":"uk"})']
+    is_part_time = name.split()[-1] == "Вечірній"
+
+    return Course(
+        name=name,
+        short_description=course_dict["description"],
+        course_type=(
+            CourseType.PART_TIME if is_part_time else CourseType.FULL_TIME
+        )
+    )
+
+
+def parse_courses_from_script_data(data: dict) -> List[Course]:
+
+    courses_data = [val for key, val in data.items() if key.startswith("Course")]
+
+    return [parse_single_course(course) for course in courses_data]
+
+
+def get_all_courses() -> List[Course]:
+    req = requests.get(URL)
+    soup = BeautifulSoup(req.content, "html.parser")
+    script = soup.select_one("#__NEXT_DATA__")
+    data = dict(json.loads(script.text))["props"]["apolloState"]
+    courses = parse_courses_from_script_data(data)
+
+    return courses
+
+
+if __name__ == "__main__":
+    courses = get_all_courses()
+    for course in courses:
+        print(course)
