6987ed71c909ed38ff419862cc59c4f0ce593f99
Yevhen Kaskov <kaskov.e@gmail.com>
2023-05-06 17:35:14 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass
 from enum import Enum
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+BASE_URL = "https://mate.academy/"
 
 
 class CourseType(Enum):
@@ -14,5 +19,42 @@ class Course:
     course_type: CourseType
 
 
+def get_single_course(section: str, course_type: CourseType) -> Course:
+    name = section.select_one(".typography_landingH3__vTjok").text
+    short_description = section.select_one(
+        ".typography_landingMainText__Ux18x"
+    ).text
+    return Course(name, short_description, course_type)
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    driver = webdriver.Chrome()
+    driver.get(BASE_URL)
+    time.sleep(15)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+
+    block_ft = soup.find("div", {"id": "full-time"})
+    sections1 = block_ft.select(".CourseCard_cardContainer__7_4lK")
+    full_time_list = [
+        get_single_course(section, CourseType.FULL_TIME)
+        for section in sections1
+    ]
+
+    block_pt = soup.find("div", {"id": "part-time"})
+    sections2 = block_pt.select(".CourseCard_cardContainer__7_4lK")
+    part_time_list = [
+        get_single_course(section, CourseType.PART_TIME)
+        for section in sections2
+    ]
+
+    full_time_list.extend(part_time_list)
+    return full_time_list
+
+
+def main() -> None:
+    print(get_all_courses())
+
+
+if __name__ == "__main__":
+    main()
