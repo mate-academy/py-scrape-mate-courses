59f0dadb20ba5512baf279b9334e3be86c20ad7e
myroslavatrschk <tereshchukm.forstudy@gmail.com>
2023-05-08 23:41:05 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,6 +1,13 @@
 from dataclasses import dataclass
 from enum import Enum
 
+from bs4 import Tag, BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+URL = "https://mate.academy/"
+
 
 class CourseType(Enum):
     FULL_TIME = "full-time"
@@ -14,5 +21,37 @@ class Course:
     course_type: CourseType
 
 
+def parse_single_course(course_soup: Tag) -> Course:
+    name = course_soup.select_one(".typography_landingH3__vTjok").text
+    short_description = course_soup.select_one(
+        ".CourseCard_courseDescription__Unsqj"
+    ).text
+    course_type = (
+        CourseType.PART_TIME if "Вечірній" in name else CourseType.FULL_TIME
+    )
+
+    return Course(
+        name=name,
+        short_description=short_description,
+        course_type=course_type
+    )
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    service = Service("usr/local/bin/chromedriver")
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(service=service, options=options)
+
+    driver.get(URL)
+
+    driver.implicitly_wait(10)
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    courses = soup.select(".CourseCard_cardContainer__7_4lK")
+
+    return [parse_single_course(course) for course in courses]
+
+
+if __name__ == "__main__":
+    print(get_all_courses())
