a89685efb4efddcc8d31b80ea90ce30ed342c43f
Eugene <eugene.lot77@gmail.com>
2023-05-11 13:55:00 +0300

    Hotfix flake8

--- app/parse.py
+++ app/parse.py
@@ -19,8 +19,12 @@ class Course:
 
 def parse_single_course(course_soup: BeautifulSoup) -> Course:
     name = course_soup.select_one(".typography_landingH3__vTjok").text
-    short_description = course_soup.select_one(".CourseCard_courseDescription__Unsqj").text
-    course_type = CourseType.PART_TIME if "Вечірній" in name else CourseType.FULL_TIME
+    short_description = course_soup.select_one(
+        ".CourseCard_courseDescription__Unsqj"
+    ).text
+    course_type = (
+        CourseType.PART_TIME if "Вечірній" in name else CourseType.FULL_TIME
+    )
 
     return Course(
         name=name,
@@ -38,5 +42,5 @@ def get_all_courses() -> list[Course]:
     return [parse_single_course(course_soup) for course_soup in courses]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(get_all_courses())
