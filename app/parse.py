3692e0e247eee777d02cb889a7b22bbf5370f2fa
Zenon <zenion@ukr.net>
2023-05-11 10:40:24 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,9 @@
+import json
 from dataclasses import dataclass
 from enum import Enum
+
+import requests
+from bs4 import BeautifulSoup
 
 
 class CourseType(Enum):
@@ -14,5 +18,24 @@ class Course:
     course_type: CourseType
 
 
-def get_all_courses() -> list[Course]:
-    pass
+def parse_single_course(course_dict: dict) -> Course:
+    name = course_dict['nameShort({"domain":"ua","lang":"uk"})']
+    time = CourseType.PART_TIME if name[-1] == "й" else CourseType.FULL_TIME
+    return Course(name=name,
+                  short_description=course_dict["description"],
+                  course_type=time)
+
+
+def get_all_courses() -> [Course]:
+    data = requests.get("https://mate.academy/")
+    soup = BeautifulSoup(data.content, "html.parser")
+    script = soup.select_one("#__NEXT_DATA__")
+    content = dict(json.loads(script.text))["props"]["apolloState"]
+    courses = [val for key, val in content.items() if key.startswith("Course")]
+
+    return [parse_single_course(course) for course in courses]
+
+
+if __name__ == "__main__":
+    for i in get_all_courses():
+        print(i)
