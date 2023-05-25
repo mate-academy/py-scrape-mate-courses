2261a32e92cd7bc2ee6ea56d2da221e304a0f8d3
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-05-25 21:59:29 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
 from enum import Enum
 
+import requests as requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://mate.academy/"
+
 
 class CourseType(Enum):
     FULL_TIME = "full-time"
@@ -14,5 +19,23 @@ class Course:
     course_type: CourseType
 
 
+def parse_single_course(course_soup: BeautifulSoup) -> Course:
+    name = course_soup.select_one(".typography_landingH3__vTjok").text
+    course_type = CourseType.PART_TIME if "Вечірній" in name \
+        else CourseType.FULL_TIME
+    return Course(
+        name=name,
+        short_description=course_soup.select_one(
+            ".typography_landingMainText__Ux18x"
+        ).text,
+        course_type=course_type
+    )
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    content = requests.get(BASE_URL).content
+    soup = BeautifulSoup(content, "html.parser")
+
+    courses = soup.select(".CourseCard_cardContainer__7_4lK")
+
+    return [parse_single_course(soup) for soup in courses]
