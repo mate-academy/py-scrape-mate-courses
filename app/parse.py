0a199c226aab8e4af2431ef00c651cc74dd3cc85
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2023-06-02 20:11:07 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,11 @@
 from dataclasses import dataclass
 from enum import Enum
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://mate.academy/"
 
 
 class CourseType(Enum):
@@ -14,5 +20,48 @@ class Course:
     course_type: CourseType
 
 
+def get_single_course(name: str, description: str) -> Course:
+    type_ = CourseType.PART_TIME if "part" in name else CourseType.FULL_TIME
+
+    return Course(
+        name=name.split("-")[0],
+        short_description=description,
+        course_type=type_
+    )
+
+
+def parse_single_page(page: str) -> Course:
+    page_content = requests.get(page).content
+    soup = BeautifulSoup(page_content, "html.parser")
+    description = soup.select_one("head title").text
+    name = page.split("/")[-1]
+
+    return get_single_course(name=name, description=description)
+
+
+def generate_parse_pages(page_links: list) -> list[Course]:
+    return [*map(parse_single_page, page_links)]
+
+
+def get_list_page_course(soup: BeautifulSoup, page_url: str) -> list[urljoin]:
+    links = soup.select(".large-8 a")
+    return [urljoin(page_url, link["href"]) for link in links]
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    page_url = BASE_URL
+    page = requests.get(page_url).content
+    soup = BeautifulSoup(page, "html.parser")
+
+    page_links = get_list_page_course(soup, page_url)
+    list_parse_of_courses = generate_parse_pages(page_links)
+
+    return list_parse_of_courses
+
+
+def main() -> None:
+    get_all_courses()
+
+
+if __name__ == "__main__":
+    main()
