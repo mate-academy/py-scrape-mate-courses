e0059ff046aef20a15afcec75f533ecd5f85f692
Viacheslav Shcherbak <32074944+SlavikSherbak@users.noreply.github.com>
2023-07-03 22:12:18 +0300

    Using OOP

--- app/parse.py
+++ app/parse.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import csv
 from dataclasses import dataclass, astuple, fields
 from enum import Enum
@@ -10,52 +12,53 @@ class CourseType(Enum):
     PART_TIME = "part-time"
 
 
+BASE_URL = "https://mate.academy/"
+
+
 @dataclass
 class Course:
     name: str
     short_description: str
     course_type: CourseType
 
-
-BASE_URL = "https://mate.academy/"
-COURSE_FIELDS = [field.name for field in fields(Course)]
-
-
-def get_single_course(
-    course_soup: BeautifulSoup,
-    course_type: CourseType
-) -> Course:
-    return Course(
-        name=course_soup.select_one("a.mb-16").text,
-        short_description=course_soup.select_one(
-            "p.typography_landingMainText__Ux18x"
-        ).text,
-        course_type=course_type
-    )
-
-
-def get_all_courses() -> list[Course]:
-    courses = []
-    page = requests.get(BASE_URL).content
-    soup = BeautifulSoup(page, "html.parser")
-
-    course_types = [
-        (
-            "#full-time section.CourseCard_cardContainer__7_4lK",
-            CourseType.FULL_TIME
-        ),
-        (
-            "#part-time section.CourseCard_cardContainer__7_4lK",
-            CourseType.PART_TIME
+    def get_single_course(
+            self,
+            course_soup: BeautifulSoup,
+            course_type: CourseType
+    ) -> Course:
+        return Course(
+            name=course_soup.select_one("a.mb-16").text,
+            short_description=course_soup.select_one(
+                "p.typography_landingMainText__Ux18x"
+            ).text,
+            course_type=course_type
         )
-    ]
 
-    for course_selector, course_type in course_types:
-        courses.extend([
-            get_single_course(course_soup, course_type)
-            for course_soup in soup.select(course_selector)
-        ])
-    return courses
+    def get_all_courses(self) -> list[Course]:
+        courses = []
+        page = requests.get(BASE_URL).content
+        soup = BeautifulSoup(page, "html.parser")
+
+        course_types = [
+            (
+                "#full-time section.CourseCard_cardContainer__7_4lK",
+                CourseType.FULL_TIME
+            ),
+            (
+                "#part-time section.CourseCard_cardContainer__7_4lK",
+                CourseType.PART_TIME
+            )
+        ]
+
+        for course_selector, course_type in course_types:
+            courses.extend([
+                self.get_single_course(self, course_soup, course_type)
+                for course_soup in soup.select(course_selector)
+            ])
+        return courses
+
+
+COURSE_FIELDS = [field.name for field in fields(Course)]
 
 
 def write_to_csv(courses: list[Course], output_csv_path: str) -> None:
@@ -66,4 +69,4 @@ def write_to_csv(courses: list[Course], output_csv_path: str) -> None:
 
 
 if __name__ == "__main__":
-    write_to_csv(get_all_courses(), "Made_Academy")
+    write_to_csv(Course.get_all_courses(Course), "Made_Academy.csv")
