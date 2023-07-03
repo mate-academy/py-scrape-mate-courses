9c6c0011b19b63d55901bcdd66398c7679a3862d
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-07-03 19:31:07 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,13 @@
 from dataclasses import dataclass
 from enum import Enum
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://mate.academy"
+
+OUTPUT_CSV_PATH = "courses.csv"
 
 
 class CourseType(Enum):
@@ -12,7 +20,70 @@ class Course:
     name: str
     short_description: str
     course_type: CourseType
+    modules: str
+    topics: str
+    duration: str
+
+
+def get_detail_info_soup(detail_url: str) -> list[int | str]:
+    page = requests.get(urljoin(BASE_URL, detail_url)).content
+    page_soup = BeautifulSoup(page, "html.parser")
+
+    modules = page_soup.select_one(
+        ".CourseModulesHeading_modulesNumber__GNdFP"
+    ).text
+    topics = page_soup.select_one(
+        ".CourseModulesHeading_topicsNumber__PXMnR"
+    ).text
+    duration = page_soup.select_one(
+        ".CourseModulesHeading_courseDuration__f_c3H"
+    ).text
+    return [modules, topics, duration]
+
+
+def get_course(soup: BeautifulSoup) -> Course:
+    name = soup.select_one(
+        "span.typography_landingH3__vTjok"
+    ).text.replace("Курс ", "")
+    detail_url = soup.select_one(".mb-16")["href"]
+    info = get_detail_info_soup(detail_url=detail_url)
+
+    if name.endswith("flex"):
+        course_type = CourseType.PART_TIME
+    else:
+        course_type = CourseType.FULL_TIME
+
+    return Course(
+        name=name,
+        short_description=soup.select_one(
+            "p.typography_landingMainText__Ux18x.CourseCard_courseDescription__Unsqj"
+        ).text,
+        course_type=course_type,
+        modules=info[0],
+        topics=info[1],
+        duration=info[2],
+    )
+
+
+def get_courses_soups_list() -> BeautifulSoup:
+    page = requests.get(BASE_URL).content
+    page_soup = BeautifulSoup(page, "html.parser")
+
+    courses = page_soup.select(".CourseCard_cardContainer__7_4lK")
+
+    return courses
 
 
 def get_all_courses() -> list[Course]:
-    pass
+    all_courses_soup = get_courses_soups_list()
+    result = []
+
+    for soup in all_courses_soup:
+        result.append(get_course(soup))
+
+    return result
+
+
+if __name__ == '__main__':
+    course_dict = get_all_courses()
+    print(course_dict)
