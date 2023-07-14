a3f5300bdf401fa3acd4c378f01fd6c7b2957e0f
Nikita Provorov <nikiyorkovich@gmail.com>
2023-07-14 18:18:42 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from enum import Enum
 
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 
 URL = "https://mate.academy/"
 
@@ -19,22 +19,21 @@ class Course:
     course_type: CourseType
 
 
-def create_single_course(soup: BeautifulSoup) -> Course:
+def create_single_course(soup: Tag) -> Course:
     name = soup.select_one(".typography_landingH3__vTjok").text.split()
-    course_type = (
-        CourseType.PART_TIME if len(name) == 3 else CourseType.FULL_TIME
-    )
 
     return Course(
         name=name[1],
         short_description=soup.select_one(
             ".CourseCard_flexContainer__dJk4p"
         ).text,
-        course_type=course_type
+        course_type=(
+            CourseType.PART_TIME if len(name) == 3 else CourseType.FULL_TIME
+        )
     )
 
 
-def get_single_data(soup: BeautifulSoup) -> list:
+def get_single_data(soup: BeautifulSoup) -> list[Course]:
     courses = soup.select(".CourseCard_cardContainer__7_4lK")
     return [create_single_course(page) for page in courses]
 
