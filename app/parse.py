d1d3458a226786ab28055cfce809465c2111ee93
OleksandrYanchuk <sashajanchuck13@gmail.com>
2023-08-02 13:43:42 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
 from enum import Enum
 
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://mate.academy/"
+
 
 class CourseType(Enum):
     FULL_TIME = "full-time"
@@ -14,5 +19,43 @@ class Course:
     course_type: CourseType
 
 
+def get_single_course(course_soup: BeautifulSoup, course_type: str) -> Course:
+    return Course(
+        name=course_soup.select_one(".typography_landingH3__vTjok").text,
+        short_description=course_soup.select_one(
+            ".typography_landingMainText__Ux18x"
+        ).text,
+        course_type=CourseType(course_type)
+    )
+
+
+def get_courses_type(
+        page_soup: BeautifulSoup,
+        course_type: str
+) -> list[Course]:
+    courses = page_soup.select(
+        f"#{course_type} .CourseCard_cardContainer__7_4lK"
+    )
+    return [get_single_course(course, course_type) for course in courses]
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    page = requests.get(BASE_URL).content
+    soup = BeautifulSoup(page, "html.parser")
+
+    all_courses = (
+        get_courses_type(
+            soup, "full-time"
+        ) + get_courses_type(soup, "part-time")
+    )
+
+    return all_courses
+
+
+if __name__ == "__main__":
+    courses = get_all_courses()
+    for course in courses:
+        print(f"Name: {course.name}")
+        print(f"Type: {course.course_type.value}")
+        print(f"Description: {course.short_description}")
+        print("--------")
