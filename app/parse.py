88bde6760c441de991d1bd8df247673689e43314
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-08-10 11:55:17 +0300

    Delete redundant commentaries

--- app/parse.py
+++ app/parse.py
@@ -6,6 +6,7 @@ import sys
 from bs4 import BeautifulSoup, Tag
 from dataclasses import dataclass, astuple, fields
 from enum import Enum
+from typing import Self
 from urllib.parse import urljoin
 
 
@@ -37,58 +38,56 @@ class Course:
     topics: int
     duration: str
 
+    @staticmethod
+    def get_course_additional_details(course_detail_url: str) -> dict:
+        course_detail_page = requests.get(
+            urljoin(BASE_URL, course_detail_url)
+        ).content
+        course_detail_soup = BeautifulSoup(course_detail_page, "html.parser")
 
-COURSE_FIELDS = [field.name for field in fields(Course)]
-
-
-def get_course_additional_details(course_detail_url: str) -> dict:
-    course_detail_page = requests.get(
-        urljoin(BASE_URL, course_detail_url)
-    ).content
-    course_detail_soup = BeautifulSoup(course_detail_page, "html.parser")
-
-    course_additional_details = dict(
-        modules=int(course_detail_soup.select_one(
-            ".CourseModulesHeading_modulesNumber__GNdFP"
-        ).text.split()[0]),
-        topics=int(course_detail_soup.select_one(
-            ".CourseModulesHeading_topicsNumber__PXMnR"
-        ).text.split()[0]),
-        duration=course_detail_soup.select_one(
-            ".CourseModulesHeading_courseDuration__f_c3H"
-        ).text,
-    )
-
-    return course_additional_details
-
-
-def get_single_course_full_details(course: Tag) -> Course:
-
-    course_detail_url = course.select_one(".mb-16")["href"]
-    course_type_in_url = course.select_one("a.mb-16")["href"].split("-")[-1]
-
-    course_name = course.select_one(".typography_landingH3__vTjok").text
-    logging.info(f"Parsing course: {course_name}")
-
-    course_base_details = dict(
-        name=course_name,
-        short_description=course.select_one(
-            "p.CourseCard_courseDescription__Unsqj"
-        ).text,
-        course_type=(
-            CourseType.PART_TIME
-            if course_type_in_url == "parttime"
-            else CourseType.FULL_TIME
+        course_additional_details = dict(
+            modules=int(course_detail_soup.select_one(
+                ".CourseModulesHeading_modulesNumber__GNdFP"
+            ).text.split()[0]),
+            topics=int(course_detail_soup.select_one(
+                ".CourseModulesHeading_topicsNumber__PXMnR"
+            ).text.split()[0]),
+            duration=course_detail_soup.select_one(
+                ".CourseModulesHeading_courseDuration__f_c3H"
+            ).text,
         )
-    )
-    course_additional_details = get_course_additional_details(
-        course_detail_url
-    )
 
-    return Course(
-        **course_base_details,
-        **course_additional_details
-    )
+        return course_additional_details
+
+    @classmethod
+    def get_single_course_full_details(cls, course: Tag) -> Self:
+        course_detail_url = course.select_one(".mb-16")["href"]
+        course_type_in_url = (
+            course.select_one("a.mb-16")["href"].split("-")[-1]
+        )
+
+        course_name = course.select_one(".typography_landingH3__vTjok").text
+        logging.info(f"Parsing course: {course_name}")
+
+        course_base_details = dict(
+            name=course_name,
+            short_description=course.select_one(
+                "p.CourseCard_courseDescription__Unsqj"
+            ).text,
+            course_type=(
+                CourseType.PART_TIME
+                if course_type_in_url == "parttime"
+                else CourseType.FULL_TIME
+            )
+        )
+        course_additional_details = cls.get_course_additional_details(
+            course_detail_url
+        )
+
+        return cls(
+            **course_base_details,
+            **course_additional_details
+        )
 
 
 def get_all_courses() -> list[Course]:
@@ -99,7 +98,7 @@ def get_all_courses() -> list[Course]:
     courses = base_soup.select(".CourseCard_cardContainer__7_4lK")
 
     parsed_courses = [
-        get_single_course_full_details(course) for course in courses
+        Course.get_single_course_full_details(course) for course in courses
     ]
 
     logging.info(
@@ -117,7 +116,7 @@ def write_courses_data_to_csv(
 ) -> None:
     with open(output_path, "w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
-        writer.writerow(COURSE_FIELDS)
+        writer.writerow(field.name for field in fields(Course))
         writer.writerows([astuple(course) for course in courses])
 
 
