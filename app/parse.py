40e9756968f4f96f757fa9a876408b3424d62788
Anastasiia <nastyashch1601@gmail.com>
2023-08-10 17:26:44 +0100

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
 from enum import Enum
+from bs4 import BeautifulSoup, Tag
+import requests
+
+
+BASE_URL = "https://mate.academy/"
 
 
 class CourseType(Enum):
@@ -14,5 +19,38 @@ class Course:
     course_type: CourseType
 
 
+QUOTE_FIELDS = [field.name for field in fields(Course)]
+
+
+def parse_single_course(course_container: Tag) -> Course:
+    name = course_container.select_one(
+        "a span.typography_landingH3__vTjok"
+    ).text.strip()
+
+    short_description = course_container.select_one(
+        "p.typography_landingMainText__Ux18x"
+    ).text.strip()
+
+    course_type = (
+        CourseType.PART_TIME
+        if "flex" in name
+        else CourseType.FULL_TIME
+    )
+
+    return Course(
+        name=name,
+        short_description=short_description,
+        course_type=course_type
+    )
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    page = requests.get(BASE_URL).content
+    soup = BeautifulSoup(page, "html.parser")
+
+    course_elements = soup.select(".CourseCard_cardContainer__7_4lK")
+
+    return [
+        parse_single_course(course_element)
+        for course_element in course_elements
+    ]
