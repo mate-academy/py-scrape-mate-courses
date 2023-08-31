9cc1d6e3ef4aa69c6906bc8c41c5de13d7630b0f
Dukh Vlad <84961728+RedCatHot@users.noreply.github.com>
2023-08-31 14:24:09 +0300

    Update parse.py

--- app/parse.py
+++ app/parse.py
@@ -18,42 +18,6 @@ class Course:
     short_description: str
     course_type: CourseType
 
-    @classmethod
-    def parse_course(cls, course: BeautifulSoup, course_type: str) -> "Course":
-        return Course(
-            name=course.select_one(".typography_landingH3__vTjok").text[5:],
-            short_description=course.select_one(
-                ".CourseCard_courseDescription__Unsqj"
-            ).text,
-            course_type=CourseType(course_type)
-        )
-
-    @classmethod
-    def get_all_course_one_type(
-            cls,
-            parsed_page: BeautifulSoup,
-            selector: str,
-            course_type: str
-    ) -> list:
-        courses = parsed_page.select(f"{selector}")
-        return [cls.parse_course(course, course_type) for course in courses]
-
 
 def get_all_courses() -> list[Course]:
-    result = []
-    parsed_page = requests.get(BASE_URL).content
-    page_soup = BeautifulSoup(parsed_page, "html.parser")
-    full_time = Course.get_all_course_one_type(
-        page_soup,
-        "#full-time > div > section.CourseCard_cardContainer__7_4lK",
-        "full-time"
-    )
-    part_time = Course.get_all_course_one_type(
-        page_soup,
-        "#part-time > div > section.CourseCard_cardContainer__7_4lK",
-        "part-time"
-    )
-    result.extend(full_time)
-    result.extend(part_time)
-
-    return result
+    pass
