eccdd2c194457066cb9c2b00df9c5c616c3edc17
Taras Borshch <tarasborshch4@gmail.com>
2023-11-08 21:17:22 +0200

    Complete py-scrape-mate-courses

--- app/parse.py
+++ app/parse.py
@@ -1,6 +1,12 @@
 from dataclasses import dataclass
 from enum import Enum
 
+import requests
+from bs4 import BeautifulSoup, Tag
+
+
+MATE_URL = "https://mate.academy/"
+
 
 class CourseType(Enum):
     FULL_TIME = "full-time"
@@ -14,5 +20,56 @@ class Course:
     course_type: CourseType
 
 
+def check_full_time_course_exists(course: Tag) -> bool:
+    if course.select_one(
+            "a[data-qa=fulltime-course-more-details-button]"
+    ) is not None:
+        return True
+    return False
+
+
+def check_part_time_course_exists(course: Tag) -> bool:
+    if course.select_one(
+            "a[data-qa=parttime-course-more-details-button]"
+    ) is not None:
+        return True
+    return False
+
+
+def get_single_course(course: Tag) -> list[Course]:
+    courses = []
+    if check_part_time_course_exists(course):
+        courses.append(
+            Course(
+                name=course.select_one(".ProfessionCard_title__Zq5ZY").text,
+                short_description=course.select(
+                    ".typography_landingTextMain__Rc8BD"
+                )[1].text,
+                course_type=CourseType.PART_TIME
+            )
+        )
+    if check_full_time_course_exists(course):
+        courses.append(
+            Course(
+                name=course.select_one(".ProfessionCard_title__Zq5ZY").text,
+                short_description=course.select(
+                    ".typography_landingTextMain__Rc8BD"
+                )[1].text,
+                course_type=CourseType.FULL_TIME
+            )
+        )
+    return courses
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    page = requests.get(MATE_URL).content
+    page_soup = BeautifulSoup(page, "html.parser")
+
+    courses = page_soup.select(".ProfessionCard_cardWrapper__JQBNJ")
+
+    all_courses = []
+
+    for course in courses:
+        all_courses.extend(get_single_course(course))
+
+    return all_courses
