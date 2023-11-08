0dec1966322eac68ac62aebba779bb22fe88c79a
Inna Isareva <inna8110302@gmail.com>
2023-11-08 19:13:32 +0200

    solution

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,10 @@
-from dataclasses import dataclass
+import csv
+from dataclasses import dataclass, fields, astuple
 from enum import Enum
+import requests
+from bs4 import BeautifulSoup, Tag
+
+BASE_URL = "https://mate.academy/"
 
 
 class CourseType(Enum):
@@ -14,5 +19,55 @@ class Course:
     course_type: CourseType
 
 
+QUOTE_FIELDS = [field.name for field in fields(Course)]
+
+
+def check_course_type(course_card: Tag) -> list[CourseType]:
+    course_types = course_card.select(
+        ".ProfessionCard_buttons__a0o60 > a[data-qa]"
+    )
+    return [
+        CourseType.FULL_TIME if "fulltime" in course_type["data-qa"]
+        else CourseType.PART_TIME
+        for course_type in course_types
+    ]
+
+
+def parse_single_course(course: Tag) -> [Course]:
+    return ([Course(
+        name=course.select_one(".ProfessionCard_title__Zq5ZY").text,
+        short_description=course.select_one(
+            ".typography_landingTextMain__Rc8BD.mb-32"
+        ).text,
+        course_type=course_type
+    )
+        for course_type in check_course_type(course)
+    ])
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    page = requests.get(BASE_URL).content
+    soup = BeautifulSoup(page, "html.parser")
+
+    courses = soup.select(".ProfessionCard_cardWrapper__JQBNJ")
+
+    all_courser = []
+    for course_soup in courses:
+        all_courser.extend(parse_single_course(course_soup))
+    return all_courser
+
+
+def write_courses_to_csv(courses: [Course], path: str) -> None:
+    with open(path, "w", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(QUOTE_FIELDS)
+        writer.writerows(astuple(course) for course in courses)
+
+
+def main(output_csv_path: str) -> None:
+    courses = get_all_courses()
+    write_courses_to_csv(courses=courses, path=output_csv_path)
+
+
+if __name__ == "__main__":
+    main("quotes.csv")
