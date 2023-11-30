f490c35d63fc4c657eb8b0d6ddc762309421b75d
Dimo-n <dimon.chmara@gmail.com>
2023-11-30 12:43:24 +0200

    Solved

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,11 @@
+import requests
+
+from bs4 import BeautifulSoup, Tag
 from dataclasses import dataclass
 from enum import Enum
+
+
+BASE_URL = "https://mate.academy/en"
 
 
 class CourseType(Enum):
@@ -14,5 +20,38 @@ class Course:
     course_type: CourseType
 
 
+def parse_single_course(course_card: Tag) -> list[Course]:
+    courses = []
+
+    name = course_card.select_one(".mb-12").text
+    short_description = course_card.select_one(".mb-32").text
+
+    course_types_soup = course_card.select("a[data-qa]")
+    for course_type in course_types_soup:
+        if "fulltime" in course_type["data-qa"]:
+            course_type = CourseType.FULL_TIME
+        elif "parttime" in course_type["data-qa"]:
+            course_type = CourseType.PART_TIME
+
+        course = Course(
+            name=name,
+            short_description=short_description,
+            course_type=course_type
+        )
+        courses.append(course)
+
+    return courses
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    courses_list = []
+
+    page = requests.get(BASE_URL).content
+    soup = BeautifulSoup(page, "html.parser")
+    course_cards = soup.select(".ProfessionCard_cardWrapper__JQBNJ")
+
+    for course_card in course_cards:
+
+        courses_list.extend(parse_single_course(course_card))
+
+    return courses_list
