f9f6009fb60533dfe96461abc378d16e95ec6fcc
mykolamateichuk <mateichukmykola@gmail.com>
2023-12-29 16:59:27 +0200

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
 from dataclasses import dataclass
 from enum import Enum
+
+
+BASE_URL = "https://mate.academy/"
 
 
 class CourseType(Enum):
@@ -14,5 +20,40 @@ class Course:
     course_type: CourseType
 
 
+def parse_single_course(soup: BeautifulSoup) -> [Course]:
+    result = [Course(
+        name=soup.select_one(".ProfessionCard_cardWrapper__JQBNJ"
+                             " > a > h3").text,
+        short_description=soup.select_one(".ProfessionCard_cardWrapper__JQBNJ"
+                                          " > .mb-32").text,
+        course_type=CourseType.PART_TIME
+    )]
+
+    if soup.select_one("a[data-qa='fulltime-course-more-details-button']"):
+        result.append(Course(
+            name=soup.select_one(".ProfessionCard_cardWrapper__JQBNJ"
+                                 " > a > h3").text,
+            short_description=soup.select_one(".ProfessionCard_cardWrapper__JQBNJ"
+                                              " > .mb-32").text,
+            course_type=CourseType.FULL_TIME
+            )
+        )
+
+    return result
+
+
+def get_courses_soups(page_soup: BeautifulSoup) -> [BeautifulSoup]:
+    return page_soup.select(".ProfessionCard_cardWrapper__JQBNJ")
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    page = requests.get(BASE_URL).content
+    soup = BeautifulSoup(page, "html.parser")
+
+    courses_soups = get_courses_soups(soup)
+
+    return [
+        course
+        for course_soup in courses_soups
+        for course in parse_single_course(course_soup)
+    ]
