042e1f0e318f5e16ef4da15a572e49faf1111e5e
Maksym Maksimov <maksymmaksimov88@gmail.com>
2024-01-15 22:06:16 +0800

    Moved parse_additional_info and parse_course to CourseParse class

--- app/parse.py
+++ app/parse.py
@@ -5,8 +5,6 @@ from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup, Tag
 
-BASE_URL = "https://mate.academy/"
-
 
 class CourseType(Enum):
     FULL_TIME = "full-time"
@@ -23,63 +21,77 @@ class Course:
     duration_in_months: int
 
 
-def parse_additional_info(course_link: str) -> tuple:
-    page = requests.get(urljoin(BASE_URL, f"{course_link}")).content
-    soup = BeautifulSoup(page, "html.parser")
+class CourseParser:
+    BASE_URL = "https://mate.academy/"
 
-    modules, topics, duration = [
-        int(additional_info.text.split()[0])
-        for additional_info in soup.select(
-            "div.CourseModulesHeading_headingGrid__50qAP p"
-        )
-    ]
-    return modules, topics, duration
+    @staticmethod
+    def parse_additional_info(course_link: str) -> tuple:
+        page = requests.get(
+            urljoin(CourseParser.BASE_URL, f"{course_link}")
+        ).content
+        soup = BeautifulSoup(page, "html.parser")
 
-
-def parse_course(course_soup: Tag) -> list[Course]:
-    courses = []
-
-    course_link_list = [
-        link["href"] for link in course_soup.select("[data-qa]")
-    ]
-
-    course_type_list = [
-        course_type.text for course_type in course_soup.select("a span")
-    ]
-
-    for course_type, course_link in zip(course_type_list, course_link_list):
-
-        modules, topics, duration = parse_additional_info(course_link)
-
-        if course_type == "Власний темп":
-            course_type = CourseType.PART_TIME
-
-        if course_type == "Повний день":
-            course_type = CourseType.FULL_TIME
-
-        courses.append(
-            Course(
-                name=course_soup.select_one("a > h3").text,
-                short_description=course_soup.select_one(
-                    "p:nth-child(3)"
-                ).text,
-                course_type=course_type,
-                modules_count=modules,
-                topics_count=topics,
-                duration_in_months=duration,
+        modules, topics, duration = [
+            int(additional_info.text.split()[0])
+            for additional_info in soup.select(
+                "div.CourseModulesHeading_headingGrid__50qAP p"
             )
-        )
-    return courses
+        ]
+        return modules, topics, duration
+
+    @staticmethod
+    def parse_course(course_soup: Tag) -> list[Course]:
+        courses = []
+
+        course_link_list = [
+            link["href"] for link in course_soup.select("[data-qa]")
+        ]
+
+        course_type_list = [
+            course_type.text for course_type in course_soup.select("a span")
+        ]
+
+        for course_type, course_link in zip(
+                course_type_list, course_link_list
+        ):
+
+            modules, topics, duration = (
+                CourseParser.parse_additional_info(course_link)
+            )
+
+            if course_type == "Власний темп":
+                course_type = CourseType.PART_TIME
+
+            if course_type == "Повний день":
+                course_type = CourseType.FULL_TIME
+
+            courses.append(
+                Course(
+                    name=course_soup.select_one("a > h3").text,
+                    short_description=course_soup.select_one(
+                        "p:nth-child(3)"
+                    ).text,
+                    course_type=course_type,
+                    modules_count=modules,
+                    topics_count=topics,
+                    duration_in_months=duration,
+                )
+            )
+        return courses
 
 
 def get_all_courses() -> list[Course]:
     all_courses = []
-    page = requests.get(BASE_URL).content
+    page = requests.get(CourseParser.BASE_URL).content
     soup = BeautifulSoup(page, "html.parser")
 
     courses = soup.select(".ProfessionCard_cardWrapper__JQBNJ")
 
-    [all_courses.extend(parse_course(course_soup)) for course_soup in courses]
+    [
+        all_courses.extend(
+            CourseParser.parse_course(course_soup)
+        ) for course_soup in courses
+    ]
 
     return all_courses
 
