ca2ca703593150443bef5684f1bdeb3aad22b1fc
serhii_loboda <serhiy.loboda@gmail.com>
2024-02-15 01:28:03 +0200

    Solution: Scrape mate courses.

--- app/parse.py
+++ app/parse.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
 from enum import Enum
 
+import requests
+from bs4 import BeautifulSoup, Tag
+
+BASE_URL = "https://mate.academy/"
+
 
 class CourseType(Enum):
     FULL_TIME = "full-time"
@@ -14,5 +19,36 @@ class Course:
     course_type: CourseType
 
 
+def get_single_course(course: Tag) -> [Course]:
+    course_name = course.select_one("a").text
+    course_description = course.select_one(".mb-32").text
+    all_course_types = course.select(".ButtonBody_buttonText__FMZEg")
+
+    course_types = {
+        "Повний день": CourseType.FULL_TIME,
+        "Власний темп": CourseType.PART_TIME,
+    }
+
+    return [
+        Course(
+            name=course_name,
+            short_description=course_description,
+            course_type=course_types[course.text],
+        ) for course in all_course_types
+    ]
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    page = requests.get(BASE_URL).content
+    soup = BeautifulSoup(page, "html.parser")
+    courses_soup = soup.select(".ProfessionCard_cardWrapper__JQBNJ")
+    all_mate_courses = []
+
+    for course in courses_soup:
+        all_mate_courses.extend(get_single_course(course))
+
+    return all_mate_courses
+
+
+if __name__ == "__main__":
+    get_all_courses()
