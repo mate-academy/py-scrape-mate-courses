a046bc76868f7363b012d1d9b3f49667260ba528
Bohdan Marko <goldenbohdanm@gmail.com>
2024-02-15 20:08:12 +0200

    Update parse.py: implement scraper for additional info

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
+from urllib.parse import urljoin
 
 import requests
 from bs4 import BeautifulSoup
@@ -17,26 +18,76 @@ class Course:
     name: str
     short_description: str
     course_type: CourseType
+    modules: int = None
+    topics: int = None
+    duration: int = None
+
+
+def get_detail_info(url: str) -> dict:
+    new_url = urljoin(BASE_URL, url)
+    page = requests.get(new_url).content
+    soup = BeautifulSoup(page, "html.parser")
+
+    modules = soup.select_one(
+        ".CourseModulesHeading_modulesNumber__UrnUh"
+        "> .CourseModulesHeading_text__bBEaP"
+    ).text.split()[0]
+    topics = soup.select_one(
+        ".CourseModulesHeading_topicsNumber__5IA8Z"
+        "> .CourseModulesHeading_text__bBEaP"
+    ).text.split()[0]
+    duration = soup.select_one(
+        ".CourseModulesHeading_courseDuration__qu2Lx"
+        "> .CourseModulesHeading_text__bBEaP"
+    ).text.split()[0]
+    return {
+        "modules": modules,
+        "topics": topics,
+        "duration": duration
+    }
+
+
+def get_single_unit_course(
+        course_soup: BeautifulSoup,
+        course_type: str
+) -> Course:
+    name = course_soup.select_one("h3").text
+    description = course_soup.select_one(".mb-32").text
+
+    choices = {
+        "Власний темп": CourseType.PART_TIME,
+        "Повний день": CourseType.FULL_TIME
+    }
+    if choices[course_type] == CourseType.PART_TIME:
+        a = course_soup.find(
+            "a", {"data-qa": "parttime-course-more-details-button"}
+        )
+        url = a.get("href")
+    else:
+        a = course_soup.find(
+            "a", {"data-qa": "fulltime-course-more-details-button"}
+        )
+        url = a.get("href")
+
+    return Course(
+        name=name,
+        short_description=description,
+        course_type=choices[course_type],
+        **get_detail_info(url),
+    )
 
 
 def get_unit_course(course_soup: BeautifulSoup) -> Course:
-    name = course_soup.select_one("h3").text
-    description = course_soup.select_one(".mb-32").text
     course_types = [
         course_type.text
         for course_type in
         course_soup.select(".ButtonBody_buttonText__FMZEg")
     ]
-    choices = {
-        "Власний темп": CourseType.PART_TIME,
-        "Повний день": CourseType.FULL_TIME
-    }
+    for url in course_soup.select("a.Button_secondary__DNIuD"):
+        get_detail_info(url["href"])
     return [
-        Course(
-            name=name,
-            short_description=description,
-            course_type=choices[course_type]
-        ) for course_type in course_types
+        get_single_unit_course(course_soup, course_type)
+        for course_type in course_types
     ]
 
 
